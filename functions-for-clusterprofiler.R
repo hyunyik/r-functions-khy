95cf21c135ad95eca4b742460288c34dd960c7c5
Hyun-Yi Kim <hyunyi.kim@gmail.com>
2021-05-17 18:16:58 +0900

    run_emap_w_ego_termsim added.

--- functions-for-clusterprofiler.R
+++ functions-for-clusterprofiler.R
@@ -1,4 +1,9 @@
 run_ego <- function(gene, ont, pvalueCutoff = 0.05, nCluster = null) {
+  require(GOSemSim)
+  require(enrichplot)
+  require(clusterProfiler)
+  require(ggplot2)
+  
   d <- godata('org.Mm.eg.db', ont = ont)
   ego <- enrichGO(gene = names(deg.sig.gene.list), OrgDb = org.Mm.eg.db, keyType = "SYMBOL", ont = ont, pvalueCutoff = 0.05)
   ego@result[ego@result$pvalue %>% order,]
@@ -16,6 +21,37 @@ run_ego <- function(gene, ont, pvalueCutoff = 0.05, nCluster = null) {
     print(pp)
     go_cls_list[paste0("Cluster ",i," GO terms")] <- pp
     write.csv(pp, file = paste0(ont, "_GO_cluster_",i ,".csv"))
+  }
+  
+  r <- list()
+  r[["enrichGO"]] <- ego
+  r[["p.GOcluster"]] <- p
+  r[["list.GOcluster"]] <- go_cls_list
+  return(r)
+}
+
+run_emap_w_ego_termsim <- function(ego, d, pvalueCutoff = 0.05, nCluster = NULL, showCategory = NULL) {
+  require(GOSemSim)
+  require(enrichplot)
+  require(clusterProfiler)
+  require(ggplot2)
+  
+  if(is.null(showCategory)) { showCategory <- dim(ego@result)[1] }
+  
+  ego <- pairwise_termsim(ego, semData = d, showCategory = showCategory)
+  p <- emapplot_cluster(ego, showCategory = dim(ego@result)[1], repel = T, cluster_num_label = T, cex_label_group = 2, nCluster = nCluster)
+  p <- p + xlim(min(p$data$x)-1,max(p$data$x)+1) +ylim(min(p$data$y)-1,max(p$data$y)+1)
+  p
+  
+  go_cls_list <- list()
+  for (i in sort(unique(p$data$color))) {
+    pp <- p$data[p$data$color==i,][,c(3, 4, 5)]
+    colnames(pp) <- c(paste0("Cluster ",i," GO terms"), "Size", "p-Value")
+    pp <- pp[order(pp$Size, decreasing = T), ]
+    rownames(pp) <- NULL
+    print(pp)
+    go_cls_list[paste0("Cluster ",i," GO terms")] <- pp
+    #write.csv(pp, file = paste0(d@ont, "_GO_cluster_",i ,".csv"))
   }
   
   r <- list()
